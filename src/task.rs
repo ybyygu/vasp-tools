eb1085810b4423b933a0d91c39ebe1828d23405d
Wenping Guo <ybyygu@gmail.com>
2021-01-05 20:49:05 +0800

    Rewrite vasp interactive results parser

--- src/task.rs
+++ src/task.rs
@@ -16,27 +16,12 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::io::LineWriter;
 
-#[derive(Debug, PartialEq, Eq, Clone)]
-enum ReadState {
-    // 可忽略
-    Skip,
-    // 当前结构forces行
-    Forces,
-    // 当前结构能量行
-    Energy,
-    // 需要在stdin写入新结构的分数坐标
-    InputPositions,
-}
-
 #[derive(Debug)]
 pub(crate) struct Task {
     child: Child,
-    stdout: Option<ChildStdout>,
+    // stdout: Option<ChildStdout>,
     stdin: Option<ChildStdin>,
-    stdout_reader: Option<BufReader<ChildStdout>>,
-
-    state: ReadState,
-    computed: VaspResult,
+    stdout: Option<BufReader<ChildStdout>>,
 }
 
 impl Task {
@@ -48,54 +33,16 @@ impl Task {
             .spawn()
             .with_context(|| format!("run script: {:?}", exe))?;
 
-        let stdout = child.stdout.take();
+        let stdout = child.stdout.take().and_then(|x| BufReader::new(x).into());
         let stdin = child.stdin.take();
-        let state = ReadState::Skip;
-        let computed = VaspResult::default();
 
-        Ok(Self {
-            child,
-            stdin,
-            stdout,
-            state,
-            computed,
-            stdout_reader: None,
-        })
+        Ok(Self { child, stdin, stdout })
     }
 }
 // base:1 ends here
 
 // [[file:../vasp-server.note::*core][core:1]]
 impl Task {
-    fn enter_state_read_forces(&mut self) {
-        info!("start reading forces");
-        self.state = ReadState::Forces;
-    }
-
-    fn enter_state_skip(&mut self) {
-        self.state = ReadState::Skip;
-    }
-
-    fn enter_state_read_energy(&mut self) {
-        info!("start reading energy");
-        self.state = ReadState::Energy;
-    }
-
-    fn enter_state_input_positions(&mut self) {
-        info!("read input structure from stdin");
-        self.state = ReadState::InputPositions;
-    }
-
-    /// Collect result line by line
-    fn collect(&mut self, line: &str) {
-        self.computed.collect(line, &self.state);
-    }
-
-    /// Reset collected results
-    fn collect_done(&mut self) {
-        self.computed = VaspResult::default();
-    }
-
     // 从POSCAR中提取分数坐标, 写入vasp stdin
     fn take_action_input(&mut self) -> Result<()> {
         use gchemol::prelude::*;
@@ -117,145 +64,96 @@ impl Task {
 
         Ok(())
     }
-
-    /// 输出当前结构对应的计算结果
-    fn take_action_output(&mut self) -> Result<()> {
-        let energy = self.computed.get_energy().expect("no energy");
-        let forces = self.computed.get_forces().expect("no forces");
-
-        // FIXME: rewrite
-        let mut mp = gosh::model::ModelProperties::default();
-        mp.set_forces(dbg!(forces));
-        mp.set_energy(dbg!(energy));
-
-        Ok(())
-    }
-
-    /// 根据当前状态, 采取对应的行动, 比如继续读取, 输入结构还是输出结果.
-    fn take_action(&mut self, line: &str) -> Result<()> {
-        dbg!(&line);
-        match self.state {
-            ReadState::InputPositions => {
-                self.take_action_input()?;
-                self.enter_state_skip();
-            }
-            ReadState::Forces => {
-                self.collect(line);
-            }
-            ReadState::Energy => {
-                self.collect(line);
-                self.take_action_output()?;
-                self.collect_done();
-            }
-            _ => {}
-        }
-
-        Ok(())
-    }
-
-    /// 开始主循环
-    fn enter_main_loop(&mut self) -> Result<()> {
-        let mut lines = BufReader::new(self.stdout.take().unwrap()).lines();
-        for cycle in 0.. {
-            if let Some(line) = lines.next() {
-                let line = line?;
-                if line == "FORCES:" {
-                    self.enter_state_read_forces();
-                } else if line.trim_start().starts_with("1 F=") {
-                    self.enter_state_read_energy();
-                } else if line == "POSITIONS: reading from stdin" {
-                    self.enter_state_input_positions();
-                }
-                self.take_action(&line)?;
-            } else {
-                break;
-            }
-        }
-
-        Ok(())
-    }
 }
 // core:1 ends here
 
 // [[file:../vasp-server.note::*vasp][vasp:1]]
-#[derive(Debug, Default, Clone)]
-struct VaspResult {
-    forces: String,
-    energy: String,
-}
+mod vasp {
+    use super::*;
+    use text_parser::parsers::*;
 
-impl VaspResult {
-    fn collect(&mut self, line: &str, state: &ReadState) {
-        use ReadState::*;
-
-        match state {
-            Forces => {
-                self.forces.push_str(line);
-                self.forces.push_str("\n");
-            }
-            Energy => {
-                self.energy.push_str(line);
-            }
-            _ => {}
+    fn parse_vasp_energy(s: &str) -> Option<f64> {
+        if s.len() < 42 {
+            None
+        } else {
+            s[26..26 + 16].trim().parse().ok()
         }
     }
 
-    fn get_energy(&self) -> Option<f64> {
-        parse_vasp_energy(&self.energy)
+    #[test]
+    fn test_parse_vasp_energy() {
+        let s = "   1 F= -.84780990E+02 E0= -.84775142E+02  d E =-.847810E+02  mag=     3.2666";
+        let (_, e) = read_energy(s).unwrap();
+        assert_eq!(e, -0.84775142E+02);
     }
 
-    fn get_forces(&self) -> Option<Vec<[f64; 3]>> {
-        parse_vasp_forces(&self.forces)
+    // FORCES:
+    //      0.2084558     0.2221942    -0.1762308
+    //     -0.1742340     0.2172782     0.2304866
+    //      0.2244132    -0.1794341     0.2106465
+    //     -0.2907316    -0.2746548    -0.2782190
+    //     -0.2941880    -0.0306001    -0.0141722
+    fn read_forces(s: &str) -> IResult<&str, Vec<[f64; 3]>> {
+        let tag_forces = tag("FORCES:");
+        let read_forces = many1(read_xyz);
+
+        do_parse!(
+            s,
+            tag_forces >> eol   >>     // FORCES:
+            forces: read_forces >>     // forces in each line
+            (forces)
+        )
     }
-}
 
-fn parse_vasp_forces(s: &str) -> Option<Vec<[f64; 3]>> {
-    if !s.starts_with("FORCES:") {
-        None
-    } else {
-        s.lines()
-            .skip(1)
-            .map(|line| {
-                let parts: Vec<_> = line
-                    .split_whitespace()
-                    .map(|p| match p.parse() {
-                        Ok(value) => value,
-                        Err(err) => {
-                            dbg!(line, err);
-                            todo!();
-                        }
-                    })
-                    .collect();
-                [parts[0], parts[1], parts[2]]
-            })
-            .collect_vec()
-            .into()
+    //      0.2084558     0.2221942    -0.1762308
+    fn read_xyz(s: &str) -> IResult<&str, [f64; 3]> {
+        do_parse!(
+            s,
+            space1 >> xyz: xyz_array >> read_line >> // ignore the remaining characters
+            (xyz)
+        )
     }
-}
 
-fn parse_vasp_energy(s: &str) -> Option<f64> {
-    if s.len() < 42 {
-        None
-    } else {
-        s[26..26 + 16].trim().parse().ok()
+    //    1 F= -.85097948E+02 E0= -.85096866E+02  d E =-.850979E+02  mag=     2.9646
+    //    2 F= -.85086257E+02 E0= -.85082618E+02  d E =-.850863E+02  mag=     2.9772
+    // POSITIONS: reading from stdin
+    fn read_energy(s: &str) -> IResult<&str, f64> {
+        let tag_nf = tag("F=");
+        let tag_e0 = tag("E0=");
+        do_parse!(
+            s,
+            space0 >> digit1 >> space1 >> tag_nf >> space0 >> double >>  // 1 F= ...
+            space0 >> tag_e0 >> space0 >> energy: double >> read_line >> // E0= ...
+            (energy)
+        )
     }
-}
 
-#[test]
-fn test_parse_vasp_energy() {
-    let s = "   1 F= -.84780990E+02 E0= -.84775142E+02  d E =-.847810E+02  mag=     3.2666";
-    let e = parse_vasp_energy(s);
-    assert_eq!(e, Some(-0.84775142E+02));
-}
+    fn read_energy_and_forces(s: &str) -> IResult<&str, (f64, Vec<[f64; 3]>)> {
+        let jump = take_until("FORCES:\n");
+        do_parse!(
+            s,
+            jump >>                 // skip leading text until found "FORCES"
+            forces: read_forces >>  // read forces
+            energy: read_energy >>  // read forces
+            ((energy, forces))
+        )
+    }
 
-fn parse_energy_and_forces(s: &str) -> Result<(f64, Vec<[f64; 3]>)> {
-    let mut lines = s.lines();
-    let txt: String = lines
-        .skip_while(|line| !line.starts_with("Forces:"))
-        .take_while(|line| !line.starts_with("   1 F="))
-        .collect();
+    pub(super) fn parse_energy_and_forces(s: &str) -> Result<(f64, Vec<[f64; 3]>)> {
+        let (_, values) = read_energy_and_forces(s).unwrap();
+        Ok(values)
+    }
 
-    todo!();
+    #[test]
+    fn test_parse_vasp_interactive() -> Result<()> {
+        let s = "./tests/files/interactive.txt";
+        let s = gut::fs::read_file(s)?;
+
+        let (e, f) = parse_energy_and_forces(&s)?;
+        assert_eq!(f.len(), 25);
+
+        Ok(())
+    }
 }
 // vasp:1 ends here
 
@@ -265,8 +163,7 @@ use gosh::model::ModelProperties;
 
 impl Task {
     pub fn compute_mol(&mut self, mol: &Molecule) -> Result<ModelProperties> {
-        // FIXME: rewrite
-        let stdout = BufReader::new(self.stdout.take().unwrap());
+        let stdout = self.stdout.as_mut().unwrap();
 
         let mut text = String::new();
         let mut lines = stdout.lines();
@@ -279,7 +176,7 @@ impl Task {
                 let line = line?;
                 writeln!(&mut text, "{}", line)?;
                 if line == "POSITIONS: reading from stdin" {
-                    let (energy, forces) = parse_energy_and_forces(&text)?;
+                    let (energy, forces) = self::vasp::parse_energy_and_forces(&text)?;
                     let mut mp = ModelProperties::default();
                     mp.set_energy(energy);
                     mp.set_forces(forces);
