8b45b0c59a06b5f66196e219819bd06049214c79
Wenping Guo <ybyygu@gmail.com>
2021-01-06 09:22:12 +0800

    Move parser codes to vasp mod

--- src/task.rs
+++ src/task.rs
@@ -1,8 +1,12 @@
 // [[file:../vasp-server.note::*imports][imports:1]]
 use gut::prelude::*;
 
+use gchemol::prelude::*;
+use gchemol::Molecule;
+use gosh::gchemol;
+
 use std::os::unix::net::UnixStream;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 // imports:1 ends here
 
 // [[file:../vasp-server.note::*constants][constants:1]]
@@ -18,40 +22,60 @@ use std::io::LineWriter;
 
 #[derive(Debug)]
 pub(crate) struct Task {
-    child: Child,
-    // stdout: Option<ChildStdout>,
+    exe: PathBuf,
+    child: Option<Child>,
     stdin: Option<ChildStdin>,
     stdout: Option<BufReader<ChildStdout>>,
 }
 
 impl Task {
-    pub(crate) fn new<P: AsRef<Path>>(exe: P) -> Result<Self> {
-        let exe = exe.as_ref();
-        let mut child = Command::new(&exe)
+    pub fn new<P: AsRef<Path>>(exe: P) -> Self {
+        let exe = exe.as_ref().to_owned();
+
+        Self {
+            exe,
+            child: None,
+            stdin: None,
+            stdout: None,
+        }
+    }
+
+    fn start_cmd(&mut self) -> Result<()> {
+        let mut child = Command::new(&self.exe)
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
             .spawn()
-            .with_context(|| format!("run script: {:?}", exe))?;
+            .with_context(|| format!("run script: {:?}", &self.exe))?;
 
         let stdout = child.stdout.take().and_then(|x| BufReader::new(x).into());
         let stdin = child.stdin.take();
 
-        Ok(Self { child, stdin, stdout })
+        self.stdin = stdin;
+        self.stdout = stdout;
+
+        Ok(())
+    }
+
+    fn is_started(&self) -> bool {
+        self.child.is_some()
     }
 }
 // base:1 ends here
 
-// [[file:../vasp-server.note::*core][core:1]]
-impl Task {
-    // 从POSCAR中提取分数坐标, 写入vasp stdin
-    fn take_action_input(&mut self) -> Result<()> {
-        use gchemol::prelude::*;
-        use gchemol::Molecule;
-        use gosh::gchemol;
+// [[file:../vasp-server.note::*stop][stop:1]]
+impl Drop for Task {
+    fn drop(&mut self) {
+        if let Err(msg) = crate::vasp::stop_vasp_server() {
+            eprintln!("Failed to stop vasp server: {:?}", msg);
+        }
+    }
+}
+// stop:1 ends here
 
-        // FIXME: read POSITIONS from POSCAR
-        debug!("Read scaled positions from POSCAR file ...");
-        let mol = Molecule::from_file("POSCAR").context("Reading POSCAR file ...")?;
+// [[file:../vasp-server.note::*input][input:1]]
+impl Task {
+    /// write scaled positions to VASP stdin
+    fn input_positions(&mut self, mol: &Molecule) -> Result<()> {
         let lines: String = mol
             .get_scaled_positions()
             .expect("lattice")
@@ -65,106 +89,21 @@ impl Task {
         Ok(())
     }
 }
-// core:1 ends here
+// input:1 ends here
 
-// [[file:../vasp-server.note::*vasp][vasp:1]]
-mod vasp {
-    use super::*;
-    use text_parser::parsers::*;
-
-    fn parse_vasp_energy(s: &str) -> Option<f64> {
-        if s.len() < 42 {
-            None
-        } else {
-            s[26..26 + 16].trim().parse().ok()
-        }
-    }
-
-    #[test]
-    fn test_parse_vasp_energy() {
-        let s = "   1 F= -.84780990E+02 E0= -.84775142E+02  d E =-.847810E+02  mag=     3.2666";
-        let (_, e) = read_energy(s).unwrap();
-        assert_eq!(e, -0.84775142E+02);
-    }
-
-    // FORCES:
-    //      0.2084558     0.2221942    -0.1762308
-    //     -0.1742340     0.2172782     0.2304866
-    //      0.2244132    -0.1794341     0.2106465
-    //     -0.2907316    -0.2746548    -0.2782190
-    //     -0.2941880    -0.0306001    -0.0141722
-    fn read_forces(s: &str) -> IResult<&str, Vec<[f64; 3]>> {
-        let tag_forces = tag("FORCES:");
-        let read_forces = many1(read_xyz);
-
-        do_parse!(
-            s,
-            tag_forces >> eol   >>     // FORCES:
-            forces: read_forces >>     // forces in each line
-            (forces)
-        )
-    }
-
-    //      0.2084558     0.2221942    -0.1762308
-    fn read_xyz(s: &str) -> IResult<&str, [f64; 3]> {
-        do_parse!(
-            s,
-            space1 >> xyz: xyz_array >> read_line >> // ignore the remaining characters
-            (xyz)
-        )
-    }
-
-    //    1 F= -.85097948E+02 E0= -.85096866E+02  d E =-.850979E+02  mag=     2.9646
-    //    2 F= -.85086257E+02 E0= -.85082618E+02  d E =-.850863E+02  mag=     2.9772
-    // POSITIONS: reading from stdin
-    fn read_energy(s: &str) -> IResult<&str, f64> {
-        let tag_nf = tag("F=");
-        let tag_e0 = tag("E0=");
-        do_parse!(
-            s,
-            space0 >> digit1 >> space1 >> tag_nf >> space0 >> double >>  // 1 F= ...
-            space0 >> tag_e0 >> space0 >> energy: double >> read_line >> // E0= ...
-            (energy)
-        )
-    }
-
-    fn read_energy_and_forces(s: &str) -> IResult<&str, (f64, Vec<[f64; 3]>)> {
-        let jump = take_until("FORCES:\n");
-        do_parse!(
-            s,
-            jump >>                 // skip leading text until found "FORCES"
-            forces: read_forces >>  // read forces
-            energy: read_energy >>  // read forces
-            ((energy, forces))
-        )
-    }
-
-    pub(super) fn parse_energy_and_forces(s: &str) -> Result<(f64, Vec<[f64; 3]>)> {
-        let (_, values) = read_energy_and_forces(s).unwrap();
-        Ok(values)
-    }
-
-    #[test]
-    fn test_parse_vasp_interactive() -> Result<()> {
-        let s = "./tests/files/interactive.txt";
-        let s = gut::fs::read_file(s)?;
-
-        let (e, f) = parse_energy_and_forces(&s)?;
-        assert_eq!(f.len(), 25);
-
-        Ok(())
-    }
-}
-// vasp:1 ends here
-
-// [[file:../vasp-server.note::*compute][compute:1]]
-use gosh::gchemol::Molecule;
+// [[file:../vasp-server.note::*compute & output][compute & output:1]]
 use gosh::model::ModelProperties;
 
 impl Task {
     pub fn compute_mol(&mut self, mol: &Molecule) -> Result<ModelProperties> {
-        let stdout = self.stdout.as_mut().unwrap();
+        if !self.is_started() {
+            mol.to_file("POSCAR").context("write POSCAR for initial calculation")?;
+            self.start_cmd()?;
+        } else {
+            self.input_positions(mol)?;
+        }
 
+        let stdout = self.stdout.as_mut().unwrap();
         let mut text = String::new();
         let mut lines = stdout.lines();
         loop {
@@ -176,7 +115,7 @@ impl Task {
                 let line = line?;
                 writeln!(&mut text, "{}", line)?;
                 if line == "POSITIONS: reading from stdin" {
-                    let (energy, forces) = self::vasp::parse_energy_and_forces(&text)?;
+                    let (energy, forces) = crate::vasp::stdout::parse_energy_and_forces(&text)?;
                     let mut mp = ModelProperties::default();
                     mp.set_energy(energy);
                     mp.set_forces(forces);
@@ -186,4 +125,4 @@ impl Task {
         }
     }
 }
-// compute:1 ends here
+// compute & output:1 ends here
--- src/vasp.rs
+++ src/vasp.rs
@@ -190,3 +190,244 @@ impl ChemicalModel for VaspServer {
     }
 }
 // pub/chemical model:1 ends here
+
+// [[file:../vasp-server.note::*INCAR file][INCAR file:1]]
+use gut::prelude::*;
+
+#[derive(Debug, Clone)]
+struct INCAR {
+    // in tag = value pair
+    params: Vec<(String, String)>,
+}
+
+impl INCAR {
+    /// Read VASP INCAR from `path`
+    pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self> {
+        // INCAR中可能会含有中文字符, 或者无效的UTF-8字符
+        use bstr::{ByteSlice, ByteVec};
+
+        let bytes = std::fs::read(path)?;
+        let lines: Vec<&[u8]> = bytes.lines().filter(|line| line.contains_str("=")).collect();
+
+        let mut final_lines = String::new();
+        // 首先剔除所有"#"号开头的注释
+        for line in lines {
+            if let Some(i) = line.find("#") {
+                line[0..i].to_str_lossy_into(&mut final_lines);
+            } else {
+                line[..].to_str_lossy_into(&mut final_lines);
+            }
+            final_lines += "\n";
+        }
+
+        let mut params: Vec<(String, String)> = vec![];
+        for line in final_lines.lines() {
+            let s: Vec<_> = line.splitn(2, "=").collect();
+            // 变成大写的TAG
+            let tag = s[0].trim();
+            // 同一行可以出现多个tag=value组合, 中间用"；"分隔
+            let value = s[1].trim();
+            if value.contains(";") {
+                warn!("; found. that is not supported.")
+            }
+            params.push((tag.to_uppercase(), value.to_string()));
+        }
+        let incar = Self { params };
+
+        Ok(incar)
+    }
+
+    /// Save as INCAR file
+    pub fn save(&self) -> Result<()> {
+        let n = self
+            .params
+            .iter()
+            .map(|(tag, _)| tag.len())
+            .max()
+            .expect("INCAR: no lines");
+
+        let lines: String = self
+            .params
+            .iter()
+            .map(|(tag, value)| format!("{:n$} = {}\n", tag, value, n = n))
+            .collect();
+
+        gut::fs::write_to_file("INCAR", &lines)?;
+        Ok(())
+    }
+}
+
+#[test]
+#[ignore]
+fn test_incar() -> Result<()> {
+    let incar = INCAR::from_file("./tests/files/INCAR")?;
+    dbg!(incar);
+
+    Ok(())
+}
+// INCAR file:1 ends here
+
+// [[file:../vasp-server.note::*update INCAR][update INCAR:1]]
+fn update_vasp_incar_file(path: &Path) -> Result<()> {
+    // INCAR file may contains invalid UTF-8 characters, so we handle it using
+    // byte string
+    use bstr::{ByteSlice, B};
+
+    let mandatory_params = vec![
+        "POTIM = 0",
+        "NELM = 200",
+        "NSW = 0",
+        "IBRION = -1",
+        "ISYM = 0",
+        "INTERACTIVE = .TRUE.",
+    ];
+
+    // remove mandatory tags defined by user, so we can add the required
+    // parameters later
+    let bytes = std::fs::read(path)?;
+    let mut lines: Vec<&[u8]> = bytes
+        .lines()
+        .filter(|line| {
+            let s = line.trim_start();
+            if !s.starts_with_str("#") && s.contains_str("=") {
+                let parts: Vec<_> = s.splitn_str(2, "=").collect();
+                if parts.len() == 2 {
+                    let tag = parts[0].trim().to_uppercase();
+                    for param in mandatory_params.iter() {
+                        let param = param.as_bytes().as_bstr();
+                        if param.starts_with(&tag) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            true
+        })
+        .collect();
+
+    // append mandatory parameters
+    lines.push(B("# Mandatory parameters for VASP server:"));
+    for param in mandatory_params.iter() {
+        lines.push(B(param));
+    }
+    let txt = bstr::join("\n", &lines);
+    println!("{}", txt.to_str_lossy());
+
+    std::fs::write("/tmp/INCAR_new", txt)?;
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn test_update_incar() -> Result<()> {
+    update_vasp_incar_file("./tests/files/INCAR".as_ref())?;
+
+    Ok(())
+}
+// update INCAR:1 ends here
+
+// [[file:../vasp-server.note::*STOPCAR][STOPCAR:1]]
+fn write_stopcar() -> Result<()> {
+    gut::fs::write_to_file("STOPCAR", "LABORT = .TRUE.\n").context("write STOPCAR")?;
+
+    Ok(())
+}
+
+pub(crate) fn stop_vasp_server() -> Result<()> {
+    write_stopcar()?;
+
+    Ok(())
+}
+// STOPCAR:1 ends here
+
+// [[file:../vasp-server.note::*stdout][stdout:1]]
+pub(crate) mod stdout {
+    use super::*;
+    use std::io::prelude::*;
+    use text_parser::parsers::*;
+
+    fn parse_vasp_energy(s: &str) -> Option<f64> {
+        if s.len() < 42 {
+            None
+        } else {
+            s[26..26 + 16].trim().parse().ok()
+        }
+    }
+
+    #[test]
+    fn test_parse_vasp_energy() {
+        let s = "   1 F= -.84780990E+02 E0= -.84775142E+02  d E =-.847810E+02  mag=     3.2666";
+        let (_, e) = read_energy(s).unwrap();
+        assert_eq!(e, -0.84775142E+02);
+    }
+
+    // FORCES:
+    //      0.2084558     0.2221942    -0.1762308
+    //     -0.1742340     0.2172782     0.2304866
+    //      0.2244132    -0.1794341     0.2106465
+    //     -0.2907316    -0.2746548    -0.2782190
+    //     -0.2941880    -0.0306001    -0.0141722
+    fn read_forces(s: &str) -> IResult<&str, Vec<[f64; 3]>> {
+        let tag_forces = tag("FORCES:");
+        let read_forces = many1(read_xyz);
+
+        do_parse!(
+            s,
+            tag_forces >> eol   >>     // FORCES:
+            forces: read_forces >>     // forces in each line
+            (forces)
+        )
+    }
+
+    //      0.2084558     0.2221942    -0.1762308
+    fn read_xyz(s: &str) -> IResult<&str, [f64; 3]> {
+        do_parse!(
+            s,
+            space1 >> xyz: xyz_array >> read_line >> // ignore the remaining characters
+            (xyz)
+        )
+    }
+
+    //    1 F= -.85097948E+02 E0= -.85096866E+02  d E =-.850979E+02  mag=     2.9646
+    //    2 F= -.85086257E+02 E0= -.85082618E+02  d E =-.850863E+02  mag=     2.9772
+    // POSITIONS: reading from stdin
+    fn read_energy(s: &str) -> IResult<&str, f64> {
+        let tag_nf = tag("F=");
+        let tag_e0 = tag("E0=");
+        do_parse!(
+            s,
+            space0 >> digit1 >> space1 >> tag_nf >> space0 >> double >>  // 1 F= ...
+            space0 >> tag_e0 >> space0 >> energy: double >> read_line >> // E0= ...
+            (energy)
+        )
+    }
+
+    fn read_energy_and_forces(s: &str) -> IResult<&str, (f64, Vec<[f64; 3]>)> {
+        let jump = take_until("FORCES:\n");
+        do_parse!(
+            s,
+            jump >>                 // skip leading text until found "FORCES"
+            forces: read_forces >>  // read forces
+            energy: read_energy >>  // read forces
+            ((energy, forces))
+        )
+    }
+
+    pub fn parse_energy_and_forces(s: &str) -> Result<(f64, Vec<[f64; 3]>)> {
+        let (_, values) = read_energy_and_forces(s).unwrap();
+        Ok(values)
+    }
+
+    #[test]
+    fn test_parse_vasp_interactive() -> Result<()> {
+        let s = "./tests/files/interactive.txt";
+        let s = gut::fs::read_file(s)?;
+
+        let (e, f) = parse_energy_and_forces(&s)?;
+        assert_eq!(f.len(), 25);
+
+        Ok(())
+    }
+}
+// stdout:1 ends here
